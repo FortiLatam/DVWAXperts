f4337931c7224ba4b79083445e109bc15d873b60
FortiLatam <97236287+FortiLatam@users.noreply.github.com>
2022-09-22 14:57:05 -0300

    Update low.php

--- vulnerabilities/sqli/source/low.php
+++ vulnerabilities/sqli/source/low.php
@@ -1,56 +1,65 @@
 <?php
 
-if( isset( $_REQUEST[ 'Submit' ] ) ) {
+if( isset( $_GET[ 'Submit' ] ) ) {
+	// Check Anti-CSRF token
+	checkToken( $_REQUEST[ 'user_token' ], $_SESSION[ 'session_token' ], 'index.php' );
+
 	// Get input
-	$id = $_REQUEST[ 'id' ];
+	$id = $_GET[ 'id' ];
 
-	switch ($_DVWA['SQLI_DB']) {
-		case MYSQL:
-			// Check database
-			$query  = "SELECT first_name, last_name FROM users WHERE user_id = '$id';";
-			$result = mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '</pre>' );
+	// Was a number entered?
+	if(is_numeric( $id )) {
+		$id = intval ($id);
+		switch ($_DVWA['SQLI_DB']) {
+			case MYSQL:
+				// Check the database
+				$data = $db->prepare( 'SELECT first_name, last_name FROM users WHERE user_id = (:id) LIMIT 1;' );
+				$data->bindParam( ':id', $id, PDO::PARAM_INT );
+				$data->execute();
+				$row = $data->fetch();
 
-			// Get results
-			while( $row = mysqli_fetch_assoc( $result ) ) {
-				// Get values
-				$first = $row["first_name"];
-				$last  = $row["last_name"];
-
-				// Feedback for end user
-				$html .= "<pre>ID: {$id}<br />First name: {$first}<br />Surname: {$last}</pre>";
-			}
-
-			mysqli_close($GLOBALS["___mysqli_ston"]);
-			break;
-		case SQLITE:
-			global $sqlite_db_connection;
-
-			#$sqlite_db_connection = new SQLite3($_DVWA['SQLITE_DB']);
-			#$sqlite_db_connection->enableExceptions(true);
-
-			$query  = "SELECT first_name, last_name FROM users WHERE user_id = '$id';";
-			#print $query;
-			try {
-				$results = $sqlite_db_connection->query($query);
-			} catch (Exception $e) {
-				echo 'Caught exception: ' . $e->getMessage();
-				exit();
-			}
-
-			if ($results) {
-				while ($row = $results->fetchArray()) {
+				// Make sure only 1 result is returned
+				if( $data->rowCount() == 1 ) {
 					// Get values
-					$first = $row["first_name"];
-					$last  = $row["last_name"];
+					$first = $row[ 'first_name' ];
+					$last  = $row[ 'last_name' ];
 
 					// Feedback for end user
 					$html .= "<pre>ID: {$id}<br />First name: {$first}<br />Surname: {$last}</pre>";
 				}
-			} else {
-				echo "Error in fetch ".$sqlite_db->lastErrorMsg();
-			}
-			break;
-	} 
+				break;
+			case SQLITE:
+				global $sqlite_db_connection;
+
+				$stmt = $sqlite_db_connection->prepare('SELECT first_name, last_name FROM users WHERE user_id = :id LIMIT 1;' );
+				$stmt->bindValue(':id',$id,SQLITE3_INTEGER);
+				$result = $stmt->execute();
+				$result->finalize();
+				if ($result !== false) {
+					// There is no way to get the number of rows returned
+					// This checks the number of columns (not rows) just
+					// as a precaution, but it won't stop someone dumping
+					// multiple rows and viewing them one at a time.
+
+					$num_columns = $result->numColumns();
+					if ($num_columns == 2) {
+						$row = $result->fetchArray();
+
+						// Get values
+						$first = $row[ 'first_name' ];
+						$last  = $row[ 'last_name' ];
+
+						// Feedback for end user
+						$html .= "<pre>ID: {$id}<br />First name: {$first}<br />Surname: {$last}</pre>";
+					}
+				}
+
+				break;
+		}
+	}
 }
+
+// Generate Anti-CSRF token
+generateSessionToken();
 
 ?>
